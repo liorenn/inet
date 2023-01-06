bee7f808bf7fba328c48a59fac8476cc6a98efae
liorenn <lior.oren06@gmail.com>
2023-01-06 21:56:07 +0200

    Revert "test"
    This reverts commit 6d51d9e67c40a9bd878c6ef95a4318bf703d5fae.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,8 +21,6 @@ export default function home(): JSX.Element {
                 View devices information and specifications
                 <br />
                 Find the apple device that matches you
-                <br />
-                hello world
               </Text>
               <Group>
                 <Button color='gray' variant='light' size='lg' radius='md'>
