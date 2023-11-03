45edf34a3e02944f3cd83eb4790ec6d45a1eb768
Lior Oren <96766585+liorenn@users.noreply.github.com>
2023-11-03 19:33:05 +0200

    units measurements

---
+++ locales/de/units.ts
@@ -0,0 +1,17 @@
+export default {
+  batterySize: 'mAh',
+  operatingSystem: '',
+  weight: 'grams',
+  releasePrice: '$',
+  height: 'mm',
+  width: 'mm',
+  depth: 'mm',
+  storage: 'GB',
+  cpu: 'core CPU',
+  gpu: 'core GPU',
+  unifiedMemory: 'GB',
+  wiredCharging: 'W',
+  wirelessCharging: 'W',
+  memory: 'GB RAM',
+  screenSize: 'Inches',
+}
