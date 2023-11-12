1f79e51747f6a6939af679aa2a4a4c04e89f4408
Lior Oren <lior.oren06@gmail.com>
2023-11-12 10:17:33 +0200

    start of admin page

--- src/pages/auth/admin.tsx
+++ src/pages/auth/admin.tsx
@@ -1,9 +1,16 @@
 import Head from 'next/head'
-import { Center, Loader, Table } from '@mantine/core'
+import {
+  Center,
+  Container,
+  Loader,
+  SegmentedControl,
+  Table,
+} from '@mantine/core'
 import { useUser, useSession } from '@supabase/auth-helpers-react'
 import { trpc } from '../../utils/trpc'
 import useTranslation from 'next-translate/useTranslation'
 import { useViewportSize } from '@mantine/hooks'
+import { useState } from 'react'
 
 function getObject<T extends object>(
   array: T[],
@@ -18,7 +25,21 @@ const Admin = () => {
   const session = useSession()
   const { t } = useTranslation('auth')
   const { height } = useViewportSize()
+  const [table, setTable] = useState('')
   const { data: tableColumns } = trpc.admin.getDeviceColumns.useQuery()
+  const { mutate } = trpc.admin.getTableData.useMutation()
+
+  function changeTableName(tableName: string) {
+    setTable(tableName)
+    mutate(
+      { tableName: tableName },
+      {
+        onSuccess(data) {
+          console.log(data)
+        },
+      }
+    )
+  }
 
   if (!(user && session)) {
     return <Center>{t('accessDeniedMessageSignIn')}</Center>
@@ -40,15 +61,41 @@ const Admin = () => {
   console.log
 
   return (
-    <Table striped highlightOnHover withBorder withColumnBorders>
-      <thead>
-        <tr>
-          {getObject(tableColumns, 'name', 'Device')?.fields.map((value) => {
-            return <th key={value.name}>{value.name}</th>
-          })}
-        </tr>
-      </thead>
-    </Table>
+    <>
+      <Container size='xl'>
+        <SegmentedControl
+          data={tableColumns.map((value) => value.name)}
+          onChange={(tableName) => changeTableName(tableName)}
+          value={table}
+          fullWidth
+          size='md'
+          radius='md'
+          mb='lg'
+        />
+        <Table striped highlightOnHover withBorder withColumnBorders>
+          <thead>
+            <tr>
+              {getObject(tableColumns, 'name', table)?.fields.map(
+                (value, index) => {
+                  return <th key={index}>{value.name}</th>
+                }
+              )}
+            </tr>
+          </thead>
+          {/* <tbody>
+            <tr>
+              {getObject(tableColumns, 'name', table)?.fields.map((value) => {
+                return (
+                  <>
+                    <td key={value.name}>{value.name}</td>
+                  </>
+                )
+              })}
+            </tr>
+          </tbody> */}
+        </Table>
+      </Container>
+    </>
   )
 }
 
--- src/trpc/routers/admin.ts
+++ src/trpc/routers/admin.ts
@@ -1,9 +1,30 @@
 import { z } from 'zod'
 import { router, publicProcedure } from '../trpc'
-import { Prisma } from '@prisma/client'
+import { Device, Prisma } from '@prisma/client'
 
 export const AdminRouter = router({
   getDeviceColumns: publicProcedure.query(async ({ ctx }) => {
+    //console.log(Prisma.dmmf.datamodel.models.map((model) => model.name))
     return Prisma.dmmf.datamodel.models
   }),
+  getTableData: publicProcedure
+    .input(z.object({ tableName: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const query = `SELECT * FROM \"${input.tableName}\"`
+      return await ctx.prisma.$queryRawUnsafe(query)
+    }),
 })
+
+// enum tableNamesss {
+//   'Camera',
+//   'Color',
+//   'User',
+//   'Comment',
+//   'DeviceUser',
+//   'DeviceColor',
+//   'Device',
+//   'DeviceConnector',
+//   'BiometricFeature',
+//   'CameraType',
+//   'DeviceType',
+// }
