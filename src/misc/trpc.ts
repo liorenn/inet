56e508ca8e46449bab6871550f59610614c26bb3
Lior Oren <lior.oren06@gmail.com>
2024-01-03 00:06:52 +0200

    added vercel url

--- src/misc/trpc.ts
+++ src/misc/trpc.ts
@@ -3,11 +3,12 @@ import { createTRPCNext } from '@trpc/next'
 import { type inferRouterInputs, type inferRouterOutputs } from '@trpc/server'
 import { type AppRouter } from '../server/routers/_app'
 import superjson from 'superjson'
+import { developmentWebsiteUrl } from '../../config'
 
 function getBaseUrl() {
   if (typeof window !== 'undefined') return '' // browser should use relative url
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}` // SSR should use vercel url
-  return `http://localhost:${process.env.PORT ?? 3000}` // dev SSR should use localhost
+  return developmentWebsiteUrl // dev SSR should use localhost
 }
 
 export const trpc = createTRPCNext<AppRouter>({
