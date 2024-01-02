44aed0f61f92ecf34cf2976424116b39cf1f8496
Lior Oren <lior.oren06@gmail.com>
2024-01-02 23:40:31 +0200

    added config to repo

---
+++ config.ts
@@ -0,0 +1,14 @@
+export const adminAccessKey = 6
+export const managerAccessKey = 9
+export const posthogDebug = false
+export const sendSoapRequest = false
+export const defaultColorSchema = 'dark'
+export const soapServerUrl = 'https://localhost:44394/Asp/WebService1.asmx'
+export const posthogToken = 'phc_AO6mpklrLXRf3Kynf1oMSBulMDxX90OiiHjKguhxo2t'
+export const posthogApiHost = 'https://app.posthog.com'
+export const supabaseUrl = 'https://dwbtkafawtzpzntudwnb.supabase.co'
+export const resendKey = 're_NYkzJuoA_HKpebBG3fLp81SB688GPnej4'
+export const fromEmail = 'onboarding@resend.dev'
+export const websiteUrl = 'http://localhost:3000'
+export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3YnRrYWZhd3R6cHpudHVkd25iIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjM2MTc0MDksImV4cCI6MTk3OTE5MzQwOX0.84xKcgBpyzJbRVpQCEm3jASkMyame_hrrcxsLVkPaeo'
+export const currrencyApiHost = 'https://api.freecurrencyapi.com/v1/latest?apikey'
